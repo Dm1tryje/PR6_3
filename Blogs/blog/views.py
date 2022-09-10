ff834f234559fd28b7a907aed231e83166f61b2d
Dm1tryje <muhametshin.mitya@yandex.ru>
2022-09-10 16:15:39 +0500

    PR6

--- Blogs/blog/views.py
+++ Blogs/blog/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from . models import Post
 
 # Create your views here.
@@ -14,3 +15,14 @@ class BlogCreateView(CreateView):
     model = Post
     template_name = 'post_new.html'
     fields = '__all__'
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    fields = ['title', 'body']
+    template_name = 'post_edit.html'
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
+
